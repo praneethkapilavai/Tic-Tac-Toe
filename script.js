c78c9d0cabd947b5a2e03d0b7d8b69323622a606
praneethkapilavai <praneethkapilavai@gmail.com>
2026-02-06 15:13:59 +0530

    ntg

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let msg = document.querySelector("#msg")
 let hideClass = document.querySelector(".msgContainer")
 let reset = document.querySelector("#reset-btn")
 let newGame = document.querySelector("#new-game")
+
 let patterns=[
-    [ 0  ,1 , 2],
-    [ 0  ,3 , 6],
-    [ 0  ,4 , 8], 
-    [ 1  ,4 , 7],
-    [ 2  ,5 , 8],
-    [ 2  ,4 , 6],
-    [ 3  ,4 , 5],
-    [ 6  ,7 , 8],
+    [ 0 ,1 , 2],
+    [ 0 ,3 , 6],
+    [ 0 ,4 , 8], 
+    [ 1 ,4 , 7],
+    [ 2 ,5 , 8],
+    [ 2 ,4 , 6],
+    [ 3 ,4 , 5],
+    [ 6 ,7 , 8],
 ];
 
 let turnX = true;
